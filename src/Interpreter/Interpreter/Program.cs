0cfc8ab6c485e9931ca741a66ca2734e5519a314
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2020-01-25 19:58:34 +0200

    Init

---
+++ src/Interpreter/Interpreter/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Interpreter
+{
+    class Program
+    {
+        /// <summary>
+        /// static method <c>Main</c> for starting Mini-PL interpeter.
+        /// </summary>
+        /// <param name="args">input parameters (source code file)</param>
+        static int Main(string[] args)
+        {
+            // user must provide path to source code file as input parameter
+            if (args.Length != 0)
+            {
+                System.Console.WriteLine("Please provide path to Mini-PL source file.\n");
+                return -1;
+            }
+
+            // check that input source code file exists
+            string sourceFilePath = args[0];
+            if (!System.IO.File.Exists(sourceFilePath))
+            {
+                System.Console.WriteLine("Invalid source file. File not found!\n");
+                return -1;
+            }
+
+            // create scanner and scan the input file
+            Scanner scanner = new Scanner(sourceFilePath);
+           
+
+            return 0;
+        }
+    }
+}
---
+++ src/Interpreter/Interpreter/Scanner.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Interpreter
+{
+    /// <summary>
+    /// class <c>Scanner</c> for scanning input file.
+    /// Scans and groups successive characters in souce code file into tokens.
+    /// </summary>
+    class Scanner
+    {
+        private string sourceFilePath;
+
+        /// <summary>
+        /// constructor for <c>Scanner</c> object.
+        /// </summary>
+        /// <param name="filepath">path to Mini-PL source code file</param>
+        public Scanner(string filepath)
+        {
+            sourceFilePath = filepath;
+        }
+
+        /// <summary>
+        /// method <c>Scan</c> for scanning file contents.
+        /// </summary>
+        public void Scan()
+        {
+            System.Console.WriteLine("Not implemented!");
+        }
+
+    }
+}
